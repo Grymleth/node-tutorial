43c33140d2da47505eb5570900201545ad0fbe79
Aevan Candelaria <aevan.candelaria@gmail.com>
2020-08-07 22:25:25 +0800

    Lesson 7

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 /*const server = */  
 app.listen(3000, () => {
@@ -11,21 +14,24 @@ app.listen(3000, () => {
 
 app.get('/', (req, res) => {
     // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname});
+    const blogsArr = [
+        {title: 'Best Doujin I\'ve ever read', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Hey, where\'s Ibaraki?', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to cause dolphin extinction', snippet: 'Lorem ipsum dolor sit amet consectetur'}
+    ];
+    res.render('index', { title: 'Home', blogs: blogsArr});
 });
 
 app.get('/about', (req, res) => {
     // res.send('<p>about</p>');
-    res.sendFile('./views/about.html', { root: __dirname});
+    res.render('about', { title: 'About'});
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new Blog'});
 });
-
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname});
+    res.render('404', { title: '404'});
 });
